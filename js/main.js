e85208216165b685cc8161c89567c63690b13f74
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2020-10-12 19:01:17 +0300

    layout css change

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@
 // about import
 
 /* <----- header -----> */
+const burgerDOM = document.querySelector('header .mob-menu-icon');
+    const menuDOM = document.querySelector('header .menu');
+    //
+    burgerDOM.addEventListener('click', () => {
+        //susirandame artimiausia tevini elementa div
+  
+            menuDOM.classList.toggle('show');
+    
+    //jeigu buvo paspausta yra true, o jeigu true tuomet remove show
+    
+    });
 
 /* <----- hero -----> */
 
