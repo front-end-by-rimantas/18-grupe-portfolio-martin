6c3444c592c49a148bc5ed52f3924deb589c1dd0
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2020-10-17 17:28:17 +0300

    menu link bottomborder

--- js/components/header/renderMeniu.js
+++ js/components/header/renderMeniu.js
@@ -7,9 +7,9 @@ function renderMeniu(selector, header){
     const size = header.length;
     for( let i = 0; i < size; i++){
         const men = header[i];
-        HTML+=`<div class="progress-bar">
-        <a class="link" href="#${men.title}">${men.title}</a>
-        <div class="pb"></div>
+        HTML+=`<div class="border"> <div class="progress-bar">
+        <a  class="link" href="#${men.title}">${men.title}</a>
+        <div class="pb"></div></div>
         </div>`;
     }
     return DOM.innerHTML = HTML;
