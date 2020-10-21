e522e832a854ec90f3b88b14d8751990ebd05f9c
Ukanotas <simonas.rukas@gmail.com>
2020-10-21 10:40:43 +0300

    fa icons and animations fixed

--- js/data/services.js
+++ js/data/services.js
@@ -1,31 +1,31 @@
 const services = [
     {
-        icon: 'fas fa-desktop',
+        icon: 'fa fa-google',
         heading: 'Search Optimization',
         text: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
     },
     {
-        icon: 'fas fa-layer-group',
+        icon: 'fa fa-flag',
         heading: 'Logo & Identity',
         text: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
     },
     {
-        icon: 'fas fa-paper-plane',
+        icon: 'fa fa-paper-plane',
         heading: 'Graphics Design',
         text: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
     },
     {
-        icon: 'fas fa-mobile-alt',
+        icon: 'fa fa-desktop',
         heading: 'Fully Responsive',
         text: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
     },
     {
-        icon: 'fas fa-tools',
+        icon: 'fa fa-sliders',
         heading: 'Advanced Options',
         text: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
     },
     {
-        icon: 'fas fa-tags',
+        icon: 'fa fa-tags',
         heading: 'Reasonable Pricing',
         text: 'The9 is a graphically polished, interactive, easily customizable, highly modern, fast loading.',
     }
