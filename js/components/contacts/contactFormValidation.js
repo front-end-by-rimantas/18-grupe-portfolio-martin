bc275a0e2bad36b4887228577a3d4cbcd4cbe0d3
ginrut <gintauter@yahoo.com>
2020-10-24 21:29:01 +0300

    verfication

--- js/components/contacts/contactFormValidation.js
+++ js/components/contacts/contactFormValidation.js
@@ -1,21 +1,19 @@
-// import { generateContacts } from `./generateCotacts.js`
-
-
 function contactFormValidationEvent() {
-    const form = e.target.querySelector('#form');
+    const form = document.querySelector('#form');
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        console.log(e);
-        const name = e.target.querySelector('#name');
-        const email = e.target.querySelector('#email');
-        const comment = e.target.querySelector('#comment');
+
+        const name = document.querySelector('#name');
+        const email = document.querySelector('#email');
+        const comment = document.querySelector('#comment');
+        // const confirmation = document.querySelector('#smallMessage');
+        // contactFormValidation(name, email, comment, confirmation);
         contactFormValidation(name, email, comment);
     });
 
 }
-
+// function contactFormValidation(name, email, comment, confirmation)
 function contactFormValidation(name, email, comment) {
-    console.log('labas');
 
     const nameValue = name.value.trim();
     const emailValue = email.value.trim();
@@ -25,42 +23,52 @@ function contactFormValidation(name, email, comment) {
         setErrorFor(name, 'Name can not be blank');
     } else {
         setSuccessFor(name);
+    }
 
+    if (emailValue === '') {
+        setErrorFor(email, 'Email can not be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email is not valid')
+    } else {
+        setSuccessFor(email);
+    }
 
-        if (emailValue === '') {
-            setErrorFor(email, 'Email can not be blank');
-        } else if (!isEmail(emailValue)) {
-            setErrorFor(email, 'Email is not valid')
-        } else {
-            setSuccessFor(email);
-        }
-
-        if (commentValue === '') {
-            setErrorFor(comment, 'Comment section can not be blank');
-        } else {
-            setSuccessFor(comment);
-        }
-
+    if (commentValue === '') {
+        setErrorFor(comment, 'Comment section can not be blank');
+    } else {
+        setSuccessFor(comment);
     }
 
 
-    function setErrorFor(input, message) {
-        const formControl = input.nextElementSibling;
-        // const formControl = input.parentElement;
-        const small = formControl.querySelector('small');
-        formControl.className = 'contactMe-form-row error';
-        small.nextElementSibling.innerText = message;
-        // small.parentElement.innerText = message;
-    }
+    // if (contactFormValidation === true) {
+    //     return setSuccessForConfirm(confirmation, 'Your message has been sent successfully. Thank You!')
+    // }
 
-    function setSuccessFor(input) {
-        const formControl = input.nextElementSibling;
-        formControl.nextElementSibling.className = 'contactMe-form-row success';
-    }
+}
 
-    function isEmail(email) {
-        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
-    }
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'contactMe-form-row error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'contactMe-form-row success';
+}
+
+function isEmail(email) {
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 };
+
+// function setSuccessForConfirm(input, message) {
+//     const formControl = input.parentElement;
+//     const small = formControl.querySelector('smallMessage');
+//     formControl.className = 'smallMessage success';
+//     small.innerText = message;
+// }
 
 export { contactFormValidationEvent }
--- js/components/contacts/generateCotacts.js
+++ js/components/contacts/generateCotacts.js
@@ -10,23 +10,24 @@ function generateContacts(cont) {
                 <input type="text" placeholder="${cont.placeholder1}" id="name">
                 <i class="fa fa-check-circle"></i>
 		          	<i class="fa fa-exclamation-circle"></i>
-                <small>Error message</small>
+                <small></small>
               </div>
 
               <div class="contactMe-form-row">
                 <input type="text" placeholder="${cont.placeholder2}" id="email">
                 <i class="fa fa-check-circle"></i>
 		          	<i class="fa fa-exclamation-circle"></i>
-                <small>Error message</small>
+                <small></small>
               </div>
 
               <div class="contactMe-form-row last-row">
                 <textarea placeholder="${cont.placeholder3}" id="comment"></textarea>
                 <i class="fa fa-check-circle"></i>
 		          	<i class="fa fa-exclamation-circle"></i>
-                <small>Error message</small>
+                <small></small>
               </div>
-              <button type="submit" class="btn btn-getInTouch">${cont.button}</button>
+              <button type="submit" class="contactMe-button btn btn-getInTouch">${cont.button}</button>
+              <span class="smallMessage></small>
             </form>`
 };
 
